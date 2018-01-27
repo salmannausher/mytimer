fb79709094f256919eb2a25bf3eadf72397bd6d4
Salman Nausher <sallu4548@gmail.com>
2018-01-27 14:09:10 +0500

    date fix

--- script.js
+++ script.js
@@ -1,39 +1,33 @@
-var target_date = new Date().getTime() + (1000*3600*408); // set the countdown date
+var target_date = new Date("Feb 13, 2018 15:37:25").getTime(); // set the countdown date
 var days, hours, minutes, seconds; // variables for time units
 var countdown = document.getElementById("tiles"); // get tag element
-function hasLocalStorage() {
-    try {
-        return 'localStorage' in window && window['localStorage'] !== null;
-    } catch(e){
-        return false;
-    }
-}
 
 getCountdown();
 
 setInterval(function () { getCountdown(); }, 1000);
 
 function getCountdown(){
+
 	// find the amount of "seconds" between now and target
 	var current_date = new Date().getTime();
+//   localStorage.setItem("current_date");
+// // Retrieve
+// document.getElementById("result").innerHTML = localStorage.getItem("current_date");
 	var seconds_left = (target_date - current_date) / 1000;
 
 	days = pad( parseInt(seconds_left / 86400) );
 	seconds_left = seconds_left % 86400;
-
+  localStorage.setItem("days",days);
 	hours = pad( parseInt(seconds_left / 3600) );
 	seconds_left = seconds_left % 3600;
-
+  localStorage.setItem("hours",hours);
 	minutes = pad( parseInt(seconds_left / 60) );
+  localStorage.setItem("minutes",minutes);
 	seconds = pad( parseInt( seconds_left % 60 ) );
+  localStorage.setItem("seconds",seconds)
 
- // var retDay,retHour,retMin,retSec;
- // retDay=localStorage.getItem('days');
- // retHour=localStorage.getItem('hours');
- // retMin=localStorage.getItem('minutes');
- // retSec=localStorage.getItem('seconds');
 	// format countdown string + set tag value
-	countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>";
+	countdown.innerHTML = "<span>" + localStorage.getItem("days") + "</span><span>" + localStorage.getItem("hours") + "</span><span>" + localStorage.getItem("minutes") + "</span><span>" + localStorage.getItem("seconds") + "</span>";
 }
 
 function pad(n) {
